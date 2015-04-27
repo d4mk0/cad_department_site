906446070c6327de2ef19caab300f9baa582c236
Damir Konkov <d4mk0f@gmail.com>
2015-04-27 10:24:52 +0300

    improve with profile editing and disciplines was added

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,5 @@
 class DocsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_doc, only: [:show, :edit, :update, :destroy]
 
   # GET /docs
