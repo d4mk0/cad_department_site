2e76015e30208cf1a92da073750e7f76b783b994
Damir Konkov <d4mk0f@gmail.com>
2015-04-30 10:58:59 +0300

    Activeadmin was connected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from SecurityError do |exception|
+    redirect_to root_url
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def authenticate_admin_user!
+    raise SecurityError unless current_user.try(:admin?)
+  end
+
+  def after_sign_in_path_for(user)
+    user.admin? ? admin_dashboard_path : root_path 
+  end
 
   protected
 
